ef583159f5ae55212d2511330d2f06362ccd608b
ScottFang <scottman608@gmail.com>
2023-03-28 12:22:54 +0800

    update

--- django_backend/deploy/setup.sh
+++ django_backend/deploy/setup.sh
@@ -3,36 +3,36 @@
 set -e
 
 # TODO: Set to URL of git repo.
-PROJECT_GIT_URL='https://github.com/Scottman625/openlayers_Django.git'
+# PROJECT_GIT_URL='https://github.com/Scottman625/openlayers_Django.git'
 
-PROJECT_BASE_PATH='/usr/local/apps'
+# PROJECT_BASE_PATH='/usr/local/apps'
 
-PROJECT_PATH='/usr/local/apps/django_backend'
+# PROJECT_PATH='/usr/local/apps/django_backend'
 
-echo "Installing dependencies..."
-apt-get update
-echo "Hello..."
-apt-get install -y python3-dev python3-venv sqlite python3-pip supervisor nginx git
+# echo "Installing dependencies..."
+# apt-get update
+# echo "Hello..."
+# apt-get install -y python3-dev python3-venv sqlite python3-pip supervisor nginx git
 
-echo 'haha!'
+# echo 'haha!'
 # Create project directorycd
 # mkdir -p $PROJECT_BASE_PATH
 # git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH
 
 # Create virtual environment
-mkdir -p $PROJECT_BASE_PATH/env
-python3 -m venv $PROJECT_BASE_PATH/env
+# mkdir -p $PROJECT_BASE_PATH/env
+# python3 -m venv $PROJECT_BASE_PATH/env
 
-apt-get install python3-pip
-echo 'test'
+# apt-get install python3-pip
+# echo 'test'
 # Install python packages
-$PROJECT_BASE_PATH/env/bin/python3 -m pip install -r $PROJECT_BASE_PATH/requirements.txt
-$PROJECT_BASE_PATH/env/bin/python3 -m pip install uwsgi
+# $PROJECT_BASE_PATH/env/bin/python3 -m pip install -r $PROJECT_BASE_PATH/requirements.txt
+# $PROJECT_BASE_PATH/env/bin/python3 -m pip install uwsgi
 
 # Run migrations and collectstatic
-cd $PROJECT_PATH
-$PROJECT_BASE_PATH/env/bin/python3 manage.py migrate
-$PROJECT_BASE_PATH/env/bin/python3 manage.py collectstatic --noinput
+# cd $PROJECT_PATH
+# $PROJECT_BASE_PATH/env/bin/python3 manage.py migrate
+# $PROJECT_BASE_PATH/env/bin/python3 manage.py collectstatic --noinput
 
 # Configure supervisor
 cp $PROJECT_PATH/deploy/supervisor_profiles_api.conf /etc/supervisor/conf.d/profiles_api.conf
