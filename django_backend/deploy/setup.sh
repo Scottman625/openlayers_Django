4c6932fae2952e87363de08435574ee41a7b0343
ScottFang <scottman608@gmail.com>
2023-03-28 11:47:17 +0800

    commit

--- django_backend/deploy/setup.sh
+++ django_backend/deploy/setup.sh
@@ -15,12 +15,12 @@ echo "Hello..."
 apt-get install -y python3-dev python3-venv sqlite python3-pip supervisor nginx git
 
 # Create project directorycd
-mkdir -p $PROJECT_BASE_PATH
-git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH
+# mkdir -p $PROJECT_BASE_PATH
+# git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH
 
 # Create virtual environment
-mkdir -p $PROJECT_BASE_PATH/env
-python3 -m venv $PROJECT_BASE_PATH/env
+# mkdir -p $PROJECT_BASE_PATH/env
+# python3 -m venv $PROJECT_BASE_PATH/env
 
 # apt-get install python3-pip
 
